78caab89c4c5a04d404f182e47b8b955cf629d39
DolapoMabawonku <dolapo.mabawonku@gmail,com>
2020-06-15 18:36:43 +0100

    a threa should be assigned to a channel

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -17,20 +17,29 @@ class ThreadTest extends TestCase
     {
         parent::setUp();
 
-        $this->thread = factory('App\Thread')->create();
+        $this->thread = create('App\Thread');
     }
 
-
     /** @test */
-    function a_thread_has_replies()
+    public function a_thread_can_make_a_string_path()
     {
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
+        $thread =  create('App\Thread');
+
+        $this->assertEquals(
+            "/threads/{$thread->channel->slug}/{$thread->id}", $thread->path()
+        );
     }
 
     /** @test */
     function a_thread_has_a_creator()
     {
         $this->assertInstanceOf('App\User', $this->thread->creator);
+    }
+
+    /** @test */
+    function a_thread_has_replies()
+    {
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
     }
 
     /** @test */
@@ -42,5 +51,13 @@ class ThreadTest extends TestCase
         ]);
 
         $this->assertCount(1, $this->thread->replies);
+    }
+
+    /** @test */
+    public function a_thread_belongs_to_a_channel()
+    {
+        $thread = create('App\Thread');
+
+        $this->assertInstanceOf('App\Channel', $thread->channel);
     }
 }
