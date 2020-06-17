f0a6933730f8fbb251ff9bef209d491cbaaeddf8
DolapoMabawonku <dolapo.mabawonku@gmail,com>
2020-06-17 15:25:16 +0100

    episode 17

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -6,7 +6,7 @@ use App\User;
 
 class ThreadFilters extends Filters
 {
-    protected $filters = ['by'];
+    protected $filters = ['by', 'popular'];
 
 
     protected function by($username)
@@ -14,5 +14,12 @@ class ThreadFilters extends Filters
         $user = User::where('name', $username)->firstOrFail();
 
         return $this->builder->where('user_id', $user->id);
+    }
+
+    protected function popular()
+    {
+        $this->builder->getQuery()->orders = [];
+
+        return $this->builder->orderBy('replies_count', 'desc');
     }
 }
