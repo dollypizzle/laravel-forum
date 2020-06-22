6aa36486741fb0d425e439ba032341e46c365797
DolapoMabawonku <dolapo.mabawonku@gmail,com>
2020-06-22 22:45:12 +0100

    episode 35

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -4,6 +4,13 @@ namespace App;
 
 trait Favoritable
 {
+    protected static function bootFavoritable()
+    {
+        static::deleting(function ($model) {
+            $model->favorites->each->delete();
+        });
+    }
+
     public function favorites()
     {
         return $this->morphMany(Favorite::class, 'favorited');
@@ -25,7 +32,7 @@ trait Favoritable
     {
         $attributes = ['user_id' => auth()->id()];
 
-        $this->favorites()->where($attributes)->delete();
+        $this->favorites()->where($attributes)->get()->each->delete();
     }
 
     public function isFavorited()
