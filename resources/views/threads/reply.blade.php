2a9100984ff8083a884a92ca3aff936a05efe13c
DolapoMabawonku <dolapo.mabawonku@gmail,com>
2020-06-22 22:30:24 +0100

    episode 34

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -8,13 +8,7 @@
                     </a> said {{ $reply->created_at->diffForHumans() }}...
                 </h6>
                 <div>
-                    <form method="POST" action="/replies/{{ $reply->id }}/favorites">
-                        {{ csrf_field() }}
-
-                        <button type="submit" class="btn btn-primary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                            {{ $reply->favorites_count }} {{ Str::plural('Favorite', $reply->favorites_count ) }}
-                        </button>
-                    </form>
+                    <favorite :reply="{{ $reply }}"></favorite>
                 </div>
             </div>
         </div>
