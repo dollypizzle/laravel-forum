7454c603cda99d3c4457e70ad75a0f226a1d6608
DolapoMabawonku <dolapo.mabawonku@gmail,com>
2020-06-24 23:12:50 +0100

    episode 44

--- app/ThreadSubscription.php
+++ app/ThreadSubscription.php
@@ -2,9 +2,31 @@
 
 namespace App;
 
+use App\Notifications\ThreadWasUpdated;
 use Illuminate\Database\Eloquent\Model;
 
 class ThreadSubscription extends Model
 {
     protected $guarded = [];
+
+
+    /**
+     * Get the user associated with the subscription.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
+
+    public function notify($reply)
+    {
+        $this->user->notify(new ThreadWasUpdated($this->thread, $reply));
+    }
 }
