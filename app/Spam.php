684629f27558febcea1689e5a6058c71ed6c5cad
DolapoMabawonku <dolapo.mabawonku@gmail,com>
2020-06-25 16:48:53 +0100

    episode 49

---
+++ app/Spam.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Inspections;
+
+class Spam
+{
+
+    protected $inspections = [
+        InvalidKeywords::class,
+        KeyHeldDown::class
+    ];
+
+
+    public function detect($body)
+    {
+        foreach ($this->inspections as $inspection) {
+            app($inspection)->detect($body);
+        }
+
+        return false;
+    }
+}
