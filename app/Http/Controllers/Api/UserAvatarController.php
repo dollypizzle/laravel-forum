f28c2f3b11f19bf5d78ad0e6f0077aa6e2e16795
DolapoMabawonku <dolapo.mabawonku@gmail,com>
2020-06-30 13:36:49 +0100

    episode 65

--- app/Http/Controllers/Api/UserAvatarController.php
+++ app/Http/Controllers/Api/UserAvatarController.php
@@ -7,16 +7,28 @@ use Illuminate\Http\Request;
 
 class UserAvatarController extends Controller
 {
+    // public function store()
+    // {
+    //     $this->validate(request(), [
+    //         'avatar' => ['required', 'image']
+    //     ]);
+
+    //     auth()->user()->update([
+    //         'avatar_path' => request()->file('avatar')->store('avatars', 'public')
+    //     ]);
+
+    //     return response([], 204);
+    // }
+
     public function store()
     {
         $this->validate(request(), [
             'avatar' => ['required', 'image']
         ]);
-
         auth()->user()->update([
-            'avatar_path' => request()->file('avatar')->store('avatars', 'public')
+            'avatar_path' => request()->file('avatar')->store('avatars', 'avatar')
         ]);
-
         return response([], 204);
     }
+
 }
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -5,19 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="page-header mb-5">
-                    <h3>
-                        {{ $profileUser->name }}
-                    </h3>
-                    @can ('update', $profileUser)
-                        <form method="POST" action="{{ route('avatar', $profileUser) }}" enctype="multipart/form-data">
-                            {{ csrf_field() }}
-
-                            <input type="file" name="avatar">
-
-                            <button type="submit" class="btn btn-info" >Add Avatar</button>
-                        </form>
-                    @endcan
-                    <img src="{{ asset($profileUser->avatar_path) }}" width="50" height="50">
+                    <avatar-form :user="{{ $profileUser }}"></avatar-form>
                 </div>
 
             @forelse ($activities as $date => $activity)
