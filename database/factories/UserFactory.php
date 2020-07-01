8a40454ddb327e40bc527acbcaad72db49723254
DolapoMabawonku <dolapo.mabawonku@gmail,com>
2020-07-01 13:45:39 +0100

    episode 75

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -40,6 +40,8 @@ $factory->state(App\User::class, 'unconfirmed', function () {
 });
 
 $factory->define(App\Thread::class, function (Faker $faker) {
+    $title = $faker->sentence;
+
     return [
         'user_id'=> function() {
             return factory('App\User')->create()->id;
@@ -47,9 +49,10 @@ $factory->define(App\Thread::class, function (Faker $faker) {
         'channel_id'=> function() {
             return factory('App\Channel')->create()->id;
         },
-        'title' => $faker->sentence,
+        'title' => $title,
         'body' => $faker->paragraph,
         'visits' => 0,
+        'slug' => Str::slug($title)
     ];
 });
 
