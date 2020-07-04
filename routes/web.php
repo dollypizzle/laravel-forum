d6c65384f6ba0aba0700fc60035ef262d7c26869
DolapoMabawonku <dolapo.mabawonku@gmail,com>
2020-07-04 19:23:48 +0100

    episode 95

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,14 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/threads', 'ThreadsController@index')->name('threads');
 Route::get('/threads/create', 'ThreadsController@create');
+Route::get('threads/search', 'SearchController@show');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 Route::patch('threads/{channel}/{thread}', 'ThreadsController@update');
 Route::delete('/threads/{channel}/{thread}', 'ThreadsController@destroy');
 Route::post('/threads', 'ThreadsController@store')->middleware('must-be-confirmed');
 Route::get('/threads/{channel}', 'ThreadsController@index');
 // Route::resource('/threads', 'ThreadsController');
+
 
 Route::post('locked-threads/{thread}', 'LockedThreadsController@store')->name('locked-threads.store')->middleware('admin');
 Route::delete('locked-threads/{thread}', 'LockedThreadsController@destroy')->name('locked-threads.destroy')->middleware('admin');
