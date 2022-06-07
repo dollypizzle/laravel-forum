edc6b02121aea2845cb4ee61872b3a8c51a9b5a4
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-07 10:26:38 +0100

    wip1

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -14,7 +14,7 @@ class ReadThreadsTest extends TestCase
     {
         parent::setUp();
 
-        $this->thread = factory('App\Thread')->create();
+        $this->thread = create('App\Thread');
     }
 
     /** @test */
