f537171ef1d998e2b226e39420e3eb09f8059724
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-07 16:38:32 +0100

    How to test validation errors

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -14,6 +14,10 @@ class RepliesController extends Controller
 
     public function store($channelId,Thread $thread)
     {
+        $this->validate(request(), [
+            'body' => 'required',
+        ]);
+
         $thread->addReply([
             'body' => request('body'),
             'user_id' => auth()->id()
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -36,4 +36,19 @@ class ParticipateInForumTest extends TestCase
              ->assertSee($reply->body);
     }
 
+        /** @test */
+        function a_reply_requires_a_body()
+        {
+
+            $this->withExceptionHandling()->signIn();
+
+            $thread = create('App\Thread');
+
+            $reply = make('App\Reply',['body' => null]);
+
+            $this->post($thread->path().'/replies', $reply->toArray())
+                 ->assertSessionHasErrors('body');
+
+        }
+
 }
