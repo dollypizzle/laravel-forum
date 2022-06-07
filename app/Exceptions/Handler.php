c2eea5bfd54b6d1ba639b7027d3b5ed7102b6ee8
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-07 12:19:22 +0100

    wip2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,7 +50,8 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if (app()->environment() === 'testing') throw $exception;
+        // if (app()->environment() === 'testing') throw $exception;
+
         return parent::render($request, $exception);
     }
 }
---
+++ resources/views/threads/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Create a New Thread</div>
+
+                <div class="card-body">
+                    <form method="POST" action="/threads">
+                        {{ csrf_field() }}
+
+                       <div class="form-group">
+                            <label for="title">Title:</label>
+                            <input type="text" class="form-control" id="title" name="title">
+                       </div>
+
+                       <div class="form-group">
+                            <label for="body">Body:</label>
+                            <textarea name="body" rows="8" class="form-control" id="body"></textarea>
+                       </div>
+
+                       <button type="submit" class="btn btn-primary">Publish</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
