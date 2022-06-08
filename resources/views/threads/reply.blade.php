34acd91c4117ae80aa051de1d6818d67c5dc175d
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-08 18:45:27 +0100

    Episode 19

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,21 @@
 <div class="card mb-2">
     <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a> said {{ $reply->created_at->diffForHumans() }}...
+        <div class="level">
+            <h6 class="flex">
+                <a href="#" >
+                    {{ $reply->owner->name }}
+                </a> said {{ $reply->created_at->diffForHumans() }}...
+            </h6>
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    {{ csrf_field() }}
+
+                    <button type="submit" class="btn btn-primary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites()->count() }} {{ Str::plural('Favorite', $reply->favorites()->count() ) }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
 
     <div class="card-body">
