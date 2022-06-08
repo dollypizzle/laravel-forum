43e76bd708bd8ba37d4e59a750f5e709aeee9226
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-08 17:37:56 +0100

    Episode 18

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Favorite;
+use App\Reply;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Reply $reply)
+    {
+        return $reply->favorite();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,4 @@ Route::post('/threads', 'ThreadsController@store');
 Route::get('/threads/{channel}', 'ThreadsController@index');
 Route::post('threads/{channel}/{thread}/replies', 'RepliesController@store');
 
+Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
