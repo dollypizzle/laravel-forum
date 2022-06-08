bb813b6f7b2e238758a022738f314a61f2793238
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-08 08:42:36 +0100

    Validation errors and old data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/threads">All Thread</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/threads/create">New Thread</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Channels
