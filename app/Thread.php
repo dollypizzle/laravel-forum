94cfeda83fbbb7f6bd76d853b4cdccb3469489a7
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-09 08:49:54 +0100

    Episode 21

--- app/Thread.php
+++ app/Thread.php
@@ -9,6 +9,8 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected $with = ['creator', 'channel'];
+
     protected static function boot()
     {
         parent::boot();
@@ -16,6 +18,10 @@ class Thread extends Model
         static::addGlobalScope('replyCount', function ($builder) {
             $builder->withCount('replies');
         });
+
+        // static::addGlobalScope('creator', function ($builder) {
+        //     $builder->with('creator');
+        // });
     }
 
     public function path()
@@ -25,9 +31,7 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+        return $this->hasMany(Reply::class);
     }
 
     public function addReply($reply)
