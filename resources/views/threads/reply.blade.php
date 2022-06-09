05c600b26f840991177f1e03a99d29e6f9818356
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-09 07:43:12 +0100

    episode 20

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -11,7 +11,7 @@
                     {{ csrf_field() }}
 
                     <button type="submit" class="btn btn-primary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                        {{ $reply->favorites()->count() }} {{ Str::plural('Favorite', $reply->favorites()->count() ) }}
+                        {{ $reply->favorites_count }} {{ Str::plural('Favorite', $reply->favorites_count ) }}
                     </button>
                 </form>
             </div>
