022d43096192b88ac1d1eaa420a302547e2e85cf
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-10 09:27:28 +0100

    Episode 26 and 27

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -7,33 +7,16 @@
                 <div class="page-header mb-5">
                     <h3>
                         {{ $profileUser->name }}
-                        <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
                     </h3>
                 </div>
 
-            @foreach ($threads as $thread)
-                <div class="card mb-5">
-                    <div class="card-header">
-                        <div class="level">
-                            <span class="flex">
-                                <a href="{{ route('profile', $thread->creator) }}">
-                                    {{ $thread->creator->name }}
-                                </a> posted:
-                                <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                            </span>
-                            <span>
-                                {{ $thread->created_at->diffForHumans() }}
-                            </span>
-                        </div>
-                    </div>
+            @foreach ($activities as $date => $activity)
+                <h3 class="card-heading">{{ $date }}</h3>
 
-                    <div class="card-body">
-                        {{ $thread->body }}
-                    </div>
-                </div>
+                @foreach ($activity as $record)
+                    @include ("profiles.activities.{$record->type}", ['activity' => $record])
+                @endforeach
             @endforeach
-
-            {{ $threads->links() }}
             </div>
 
         </div>
--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -6,6 +6,7 @@ namespace Tests\Feature;
 use App\Activity;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Carbon\Carbon;
 
 
 class ActivityTest extends TestCase
@@ -40,4 +41,24 @@ class ActivityTest extends TestCase
 
         $this->assertEquals(2, Activity::count());
     }
+
+        /** @test */
+        function it_fetches_a_feed_for_any_user()
+        {
+            $this->signIn();
+
+            create('App\Thread', ['user_id' => auth()->id()], 2);
+
+            auth()->user()->activity()->first()->update(['created_at' => Carbon::now()->subWeek()]);
+
+            $feed = Activity::feed(auth()->user(), 50);
+
+            $this->assertTrue($feed->keys()->contains(
+                Carbon::now()->format('Y-m-d')
+            ));
+
+            $this->assertTrue($feed->keys()->contains(
+                Carbon::now()->subWeek()->format('Y-m-d')
+            ));
+        }
 }
