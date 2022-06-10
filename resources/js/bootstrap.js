84c5b5406370b07d64ba2cdb6f67faf582e0f242
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-10 12:29:21 +0100

    Episode 29

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,19 +23,9 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+window.Vue = require('vue');
+window.events = new Vue();
 
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.flash = function (message) {
+    window.events.$emit('flash', message);
+}
