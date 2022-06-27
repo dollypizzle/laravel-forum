c856212b49304f75acba170e8065841f4c307ab8
dollypizzle <dolapo.mabawonku@gmail.com>
2022-06-27 10:23:52 +0100

    episode 41

---
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SubscribeToThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_subscribe_to_threads()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $this->post($thread->path() . '/subscriptions');
+
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply here'
+        ]);
+
+        // $this->assertCount(1, auth()->user()->notifications);
+    }
+
+    /** @test */
+    public function a_user_can_unsubscribe_from_threads()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $thread->subscribe();
+
+        $this->delete($thread->path() . '/subscriptions');
+
+        $this->assertCount(0, $thread->subscriptions);
+    }
+}
