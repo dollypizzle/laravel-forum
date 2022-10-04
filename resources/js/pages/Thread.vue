5f873e770ced4709534d52ebb9ee1c015de370e1
dollypizzle <dolapo.mabawonku@gmail.com>
2022-10-04 08:52:31 +0100

    episode 42

--- resources/js/pages/Thread.vue
+++ resources/js/pages/Thread.vue
@@ -1,5 +1,7 @@
 <script>
     import Replies from '../components/Replies.vue';
+    import SubscribeButton from '../components/SubscribeButton.vue';
+
     export default {
         props: ['initialRepliesCount'],
         components: {Replies},
