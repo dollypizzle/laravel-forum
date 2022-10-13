76c1407a3651414243698ef223b7d9a8b70f3ef1
dollypizzle <dolapo.mabawonku@gmail.com>
2022-10-13 10:49:58 +0100

    Episode 59

--- app/Reply.php
+++ app/Reply.php
@@ -46,7 +46,7 @@ class Reply extends Model
 
     public function mentionedUsers()
     {
-        preg_match_all('/\@([^\s\.]+)/', $this->body, $matches);
+        preg_match_all('/@([\w\-]+)/', $this->body, $matches);
 
         return $matches[1];
     }
@@ -54,6 +54,15 @@ class Reply extends Model
     public function path()
     {
         return $this->thread->path() . "#reply-{$this->id}";
+    }
+
+    public function setBodyAttribute($body)
+    {
+        $this->attributes['body'] = preg_replace(
+            '/@([\w\-]+)/',
+            '<a href="/profiles/$1">$0</a>',
+            $body
+        );
     }
 
 }
