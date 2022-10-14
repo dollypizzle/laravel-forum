25a95692eec1cc98af7e4ed0429ebfcc8ae80405
dollypizzle <dolapo.mabawonku@gmail.com>
2022-10-14 15:32:56 +0100

    Episode 67

--- app/Trending.php
+++ app/Trending.php
@@ -6,21 +6,12 @@ use Illuminate\Support\Facades\Redis;
 
 class Trending
 {
-    /**
-     * Fetch all trending threads.
-     *
-     * @return array
-     */
+
     public function get()
     {
         return array_map('json_decode', Redis::zrevrange($this->cacheKey(), 0, 4));
     }
 
-    /**
-     * Push a new thread to the trending list.
-     *
-     * @param Thread $thread
-     */
     public function push($thread)
     {
         Redis::zincrby($this->cacheKey(), 1, json_encode([
@@ -29,11 +20,7 @@ class Trending
         ]));
     }
 
-    /**
-     * Get the cache key name.
-     *
-     * @return string
-     */
+
     public function cacheKey()
     {
         return app()->environment('testing')
@@ -41,9 +28,7 @@ class Trending
             : 'trending_threads';
     }
 
-    /**
-     * Reset all trending threads.
-     */
+
     public function reset()
     {
         Redis::del($this->cacheKey());
