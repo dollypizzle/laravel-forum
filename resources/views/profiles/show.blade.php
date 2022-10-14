45c9eedcd63438177516712a812c6c2d8d186727
dollypizzle <dolapo.mabawonku@gmail.com>
2022-10-14 08:25:05 +0100

    Episode 64

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -8,6 +8,16 @@
                     <h3>
                         {{ $profileUser->name }}
                     </h3>
+                    @can ('update', $profileUser)
+                        <form method="POST" action="{{ route('avatar', $profileUser) }}" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+
+                            <input type="file" name="avatar">
+
+                            <button type="submit" class="btn btn-info" >Add Avatar</button>
+                        </form>
+                    @endcan
+                    <img src="{{ asset($profileUser->avatar_path) }}" width="50" height="50">
                 </div>
 
             @forelse ($activities as $date => $activity)
