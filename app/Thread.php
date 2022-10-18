6604bf839834c9dca48fda419acbcea25329119d
dollypizzle <dolapo.mabawonku@gmail.com>
2022-10-18 08:44:33 +0100

    Episode 77

--- app/Thread.php
+++ app/Thread.php
@@ -26,6 +26,10 @@ class Thread extends Model
             $thread->replies->each->delete();
         });
 
+        static::created(function ($thread) {
+            $thread->update(['slug' => $thread->title]);
+        });
+
     }
 
     public function path()
@@ -104,24 +108,13 @@ class Thread extends Model
 
     public function setSlugAttribute($value)
     {
-        if (static::whereSlug($slug = Str::slug($value))->exists()) {
-            $slug = $this->incrementSlug($slug);
+        $slug = Str::slug($value);
+
+        if (static::whereSlug($slug)->exists()) {
+            $slug = "{$slug}-{$this->id}";
         }
 
         $this->attributes['slug'] = $slug;
-    }
-
-    protected function incrementSlug($slug)
-    {
-        $max = static::whereTitle($this->title)->latest('id')->value('slug');
-
-        if (is_numeric($max[-1])) {
-            return preg_replace_callback('/(\d+)$/', function ($matches) {
-                return $matches[1] + 1;
-            }, $max);
-        }
-
-        return "{$slug}-2";
     }
 
     // public function visits()
