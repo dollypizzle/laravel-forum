506577da92e008883e18e5108a31bfda5dbe45ec
dollypizzle <dolapo.mabawonku@gmail.com>
2022-10-25 11:49:23 +0100

    Episode 93

--- resources/js/pages/Thread.vue
+++ resources/js/pages/Thread.vue
@@ -25,12 +25,34 @@
             }
         },
 
+        created () {
+            this.resetForm();
+        },
+
+
         methods: {
             toggleLock () {
-                axios[
-                    this.locked ? 'delete' : 'post'
-                ]('/locked-threads/' + this.thread.slug);
+                let uri = `/locked-threads/${this.thread.slug}`;
+
+                axios[this.locked ? 'delete' : 'post'](uri);
+
                 this.locked = ! this.locked;
+            },
+            update () {
+                let uri = `/threads/${this.thread.channel.slug}/${this.thread.slug}`;
+                axios.patch(uri, this.form).then(() => {
+                    this.editing = false;
+                    this.title = this.form.title;
+                    this.body = this.form.body;
+                    flash('Your thread has been updated.');
+                })
+            },
+            resetForm () {
+                this.form = {
+                    title: this.thread.title,
+                    body: this.thread.body
+                };
+                this.editing = false;
             }
         }
     }
